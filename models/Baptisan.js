377c11131b851dfef72309dc8029f9f8f479374a
dominikusvieri <dominikusvieri9@gmail.com>
2023-06-01 15:00:56 +0700

    pembaptisan

---
+++ models/Baptisan.js
@@ -0,0 +1,51 @@
+const {Model, DataTypes} = require('sequelize')
+const sequelize = require('../db.config')
+
+class Baptisan extends Model{}
+
+Baptisan.init({
+    id:{
+        type:DataTypes.INTEGER,
+        allowNull:false,
+        autoIncrement:true,
+        primaryKey:true
+    },
+    foto_ktp:{
+        type:DataTypes.STRING
+    },
+    foto_pribadi:{
+        type:DataTypes.STRING
+    },
+    nama:{
+        type:DataTypes.STRING
+    },
+    tempat_lahir:{
+        type:DataTypes.STRING
+    },
+    tanggal_lahir:{
+        type:DataTypes.DATEONLY
+    },
+    alamat:{
+        type:DataTypes.TEXT
+    },
+    pendidikan:{
+        type:DataTypes.STRING
+    },
+    status_perkawinan:{
+        type:DataTypes.STRING
+    },
+    nama_ayah_or_wali:{
+        type:DataTypes.STRING
+    },
+    nama_ibu_or_wali:{
+        type:DataTypes.STRING
+    },
+    alamat_ortu_or_wali:{
+        type:DataTypes.STRING
+    },
+},{
+    sequelize,
+    modelName:'Baptisan'
+})
+
+module.exports = Baptisan
---
+++ routes/Baptisan.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const Baptisan = require('../models/Baptisan')
+
+router.get('/', async (req, res) => {
+    const baptisan = await Baptisan.findAll()
+
+    res.status(200).json({
+        baptisan,
+        metadata: 'baptisan Data'
+    })
+})
+
+router.post('/', async(req, res)=>{
+    const { foto_ktp, foto_pribadi, nama, tempat_lahir, tanggal_lahir, alamat, nama_ibu, pendidikan, status_perkawinan, nama_ayah_or_wali, nama_ibu_or_wali, alamat_ortu_or_wali} = req.body
+
+    const baptisan = await Baptisan.create({
+        foto_ktp, foto_pribadi, nama, tempat_lahir, tanggal_lahir, alamat, nama_ibu, pendidikan, status_perkawinan, nama_ayah_or_wali, nama_ibu_or_wali, alamat_ortu_or_wali
+    })
+
+    res.status(200).json({
+        registered: baptisan,
+        metadata: "berhasil ditambahkan"
+    })
+})
+
+module.exports = router
